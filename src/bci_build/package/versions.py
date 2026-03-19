094d0ea66524a6a4bf8e3b5994a7ea5df50a336b
Alexandre Vicenzi <alexandre.vicenzi@suse.com>
2026-03-19 16:15:58 +0100

    Support release version in package_versions.json
    Allow package_versions.json to also include the release version.
    
    For this to work properly, we need to fetch the package information from
    SCC instead of OBS.

--- src/bci_build/package/versions.py
+++ src/bci_build/package/versions.py
@@ -66,17 +66,17 @@ the current version for every code stream that is present in the dictionary.
 import json
 import xml.etree.ElementTree as ET
 from functools import cache
+from functools import cmp_to_key
 from pathlib import Path
 from typing import Dict
 from typing import List
-from typing import Literal
-from typing import NoReturn
 from typing import TypedDict
-from typing import overload
 
 import requests
 from packaging import version
+from version_utils import rpm
 
+from bci_build.container_attributes import Arch
 from bci_build.os_version import OsVersion
 from bci_build.util import ParseVersion
 
@@ -90,6 +90,7 @@ class PackageInfo(TypedDict, total=False):
     codestreams: Dict[str, CodestreamInfo]
     track_versions: bool
     version_format: str
+    package_arch: str
 
 
 PackageVersions = Dict[str, PackageInfo]
@@ -152,22 +153,8 @@ _OBS_PROJECTS: dict[OsVersion, str] = {
 } | {OsVersion(ver): f"SUSE:SLE-15-SP{ver}:Update" for ver in range(4, 8)}
 
 
-@overload
-def format_version(
-    ver: str,
-    format: Literal[ParseVersion.MAJOR, ParseVersion.MINOR, ParseVersion.PATCH],
-) -> str: ...
-
-
-@overload
-def format_version(
-    ver: str,
-    format: Literal[ParseVersion.PATCH_UPDATE, ParseVersion.OFFSET],
-) -> NoReturn: ...
-
-
-def format_version(ver: str, format: ParseVersion) -> str:
-    """Format the string `ver` to the supplied `format`, e.g.:
+def format_version(package_version: str, version_format: ParseVersion) -> str:
+    """Format the string `package_version` to the supplied `version_format`, e.g.:
 
     >>> format_version('1.2.3', ParseVersion.MAJOR)
     1
@@ -177,27 +164,105 @@ def format_version(ver: str, format: ParseVersion) -> str:
     1.2.0
 
     """
-    v = version.parse(ver.replace(":", "!").partition("~")[0])
-    match format:
+    if "-" in package_version:
+        base, rel = package_version.rsplit("-", 1)
+        release, build = rel.rsplit(".", 1)
+    else:
+        base, release, build = package_version, "1", "1"
+
+    v = version.parse(base.replace(":", "!").partition("~")[0])
+    update = v.release[3] if len(v.release) >= 4 else 0
+
+    match version_format:
         case ParseVersion.MAJOR:
-            return str(v.major)
+            return f"{v.major}"
         case ParseVersion.MINOR:
             return f"{v.major}.{v.minor}"
         case ParseVersion.PATCH:
             return f"{v.major}.{v.minor}.{v.micro}"
+        case ParseVersion.PATCH_UPDATE:
+            return f"{v.major}.{v.minor}.{v.micro}.{update}"
+        case ParseVersion.RELEASE:
+            return f"{v.major}.{v.minor}.{v.micro}-{release}"
+        case ParseVersion.RELEASE_INCREMENT:
+            return f"{v.major}.{v.minor}.{v.micro}-{release}.{build}"
         case _:
-            raise ValueError(f"Invalid version format: {format}")
+            raise ValueError(f"Invalid version format: {version_format}")
 
 
-def fetch_package_version(prj: str, pkg: str) -> str:
-    """Ask the OBS API to parse the source spec file version and return it."""
-    fetch = requests.get(
-        f"https://api.opensuse.org/public/source/{prj}/{pkg}",
-        params={"view": "info", "parse": "1"},
+def fetch_package_version_from_obs(os_version: OsVersion, package: str) -> str:
+    """Fetch the package version from the OBS API and return it."""
+    project = _OBS_PROJECTS[os_version]
+    try:
+        res = requests.get(
+            f"https://api.opensuse.org/public/source/{project}/{package}",
+            params={"view": "info", "parse": "1"},
+            headers={"User-Agent": "BCI update-versions"},
+        )
+        res.raise_for_status()
+
+        el = ET.fromstring(res.text)
+        return el.findtext("version")
+    except Exception as e:
+        raise ValueError(
+            f"Failed to get package version from OBS for '{project}/{package}': {str(e)}"
+        )
+
+
+def find_scc_product_id(os_version: OsVersion, arch: Arch):
+    """Fetch the SCC product for a given version/arch from the SCC API and return it."""
+    if os_version == OsVersion.TUMBLEWEED:
+        raise ValueError("Tumbleweed is not a registered SCC product")
+
+    res = requests.get(
+        "https://scc.suse.com/api/package_search/products",
         headers={"User-Agent": "BCI update-versions"},
     )
-    fetch.raise_for_status()
-    return getattr(ET.fromstring(fetch.text).find("version"), "text")
+
+    res.raise_for_status()
+    products = res.json()["data"]
+    identifier = f"SLES/{os_version.os_version}/{arch}"
+
+    for p in products:
+        if p["identifier"] == identifier:
+            return p["id"]
+
+    raise ValueError(f"SCC product not found: {identifier}")
+
+
+def fetch_package_versions_from_scc(
+    os_version: OsVersion, package: str, arch: Arch
+) -> list[str]:
+    """Fetch the package version from the SCC API and return it."""
+    product = find_scc_product_id(os_version, arch)
+    try:
+        res = requests.get(
+            f"https://scc.suse.com/api/package_search/packages?product_id={product}&query={package}",
+            headers={"User-Agent": "BCI update-versions"},
+        )
+
+        res.raise_for_status()
+        packages = res.json()["data"]
+
+        versions = [
+            f"{pkg['version']}-{pkg['release']}"
+            for pkg in packages
+            if pkg["name"] == package
+        ]
+        return sorted(versions, key=cmp_to_key(lambda a, b: rpm.compare_versions(a, b)))
+    except Exception as e:
+        raise ValueError(
+            f"Failed to get package version from SCC for '{os_version.os_version}/{arch}/{package}': {str(e)}"
+        )
+
+
+def fetch_package_version(
+    version_format: ParseVersion, os_version: str, package: str, arch: Arch
+):
+    if version_format in {ParseVersion.RELEASE, ParseVersion.RELEASE_INCREMENT}:
+        return fetch_package_versions_from_scc(os_version, package, arch)[-1]
+    else:
+        return fetch_package_version_from_obs(os_version, package)
 
 
 def update_versions() -> dict[str, dict[str, str]]:
@@ -211,13 +276,14 @@ def update_versions() -> dict[str, dict[str, str]]:
     for pkg_name, pkg_info in package_versions.items():
         codestreams: dict[str, CodestreamInfo] = pkg_info["codestreams"]
         version_format: ParseVersion = ParseVersion(pkg_info["version_format"])
+        pkg_arch: Arch = Arch(pkg_info.get("package_arch", "x86_64"))
 
         for os_version, _ in codestreams.items():
             pkg_version: str = fetch_package_version(
-                prj=_OBS_PROJECTS[OsVersion.parse(os_version)], pkg=pkg_name
+                version_format, OsVersion.parse(os_version), pkg_name, pkg_arch
             )
 
-            new_version = format_version(pkg_version, version_format)
+            new_version: str = format_version(pkg_version, version_format)
             codestreams[os_version]["latest"] = new_version
 
             if pkg_info.get("track_versions", False):
